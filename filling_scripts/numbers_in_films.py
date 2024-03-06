0a820e414a2a6ceeeb47ab7647480b8eacb32ea5
Danya <dan.shell@bk.ru>
2024-03-06 17:01:48 +0300

    reformat numbers.py

--- filling_scripts/numbers_in_films.py
+++ filling_scripts/numbers_in_films.py
@@ -1,27 +1,28 @@
 import sqlite3
 import random
 
-# Создаем подключение к базе данных
+# Connect to the SQLite database
 conn = sqlite3.connect('/home/kali/Documents/education/HOD/hod.db')
 cursor = conn.cursor()
 
-# Создаем курсор для выполнения операций с базой данных
-cursor = conn.cursor()
-
+# Drop the 'numbers_in_films' table if it exists
 cursor.execute('''DROP TABLE IF EXISTS numbers_in_films''')
-# Создаем таблицу
-cursor.execute('''CREATE TABLE IF NOT EXISTS numbers_in_films
-                    (film_id INTEGER PRIMARY KEY ,
-                    rating integer,
-                    budget_in_millions integer)''')
-for i in range(20):
-    rating=random.randint(30,95)/10
-    budget=random.randint(30,150)
-    cursor.execute("INSERT INTO numbers_in_films (rating,budget_in_millions) VALUES (?,?)", (rating,budget,))
 
-cursor.execute("INSERT INTO numbers_in_films (rating,budget_in_millions) VALUES (?,?)", (3,budget,))
-# Фиксируем изменения в базе данных
+# Create the 'numbers_in_films' table
+cursor.execute('''CREATE TABLE IF NOT EXISTS numbers_in_films
+                    (film_id INTEGER PRIMARY KEY,
+                    rating REAL,
+                    budget_in_millions INTEGER)
+                  ''')
+
+# Generate and insert random data into the 'numbers_in_films' table
+for _ in range(20):
+    rating = round(random.uniform(3.0, 9.5), 1)  # Generate a random floating-point number for rating
+    budget = random.randint(30, 150)  # Generate a random integer for budget
+    cursor.execute("INSERT INTO numbers_in_films (rating, budget_in_millions) VALUES (?, ?)", (rating, budget))
+
+# Commit the changes to the database
 conn.commit()
 
-# Закрываем соединение с базой данных
+# Close the database connection
 conn.close()
